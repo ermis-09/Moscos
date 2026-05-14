530081eb89cc8cad45804e31ba5feae6e4a73766
ermis-09 <turnintoturneod@gmail.com>
2026-05-14 14:29:27 +0300

    Delete config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAzLjPbJsMdKWj0kctPXioVQPFYcDR46-g",
-  authDomain: "mobil-soru.firebaseapp.com",
-  projectId: "mobil-soru",
-  storageBucket: "mobil-soru.firebasestorage.app",
-  messagingSenderId: "844021041289",
-  appId: "1:844021041289:web:92955c8f24fa606f729e58"
-};
