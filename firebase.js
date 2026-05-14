81cbdfd344574745467004709f28db411fa9260c
ermis-09 <turnintoturneod@gmail.com>
2026-05-14 14:27:28 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,6 @@
-/* ============================================
-   FIREBASE BAŞLATMA
-   ============================================ */
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAzLjPbJsMdKWj0kctPXioVQPFYcDR46-g",
-  authDomain: "mobil-soru.firebaseapp.com",
-  projectId: "mobil-soru",
-  storageBucket: "mobil-soru.firebasestorage.app",
-  messagingSenderId: "844021041289",
-  appId: "1:844021041289:web:92955c8f24fa606f729e58"
-};
+import { firebaseConfig } from './js/config.js';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
